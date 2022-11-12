79b8623e15c3a79c1fbc41af1b5dede51c6e7d6f
Ashish Kandu <ashishkandu43@gmail.com>
2022-11-13 00:18:22 +0530

    plan to add search functionality

--- variables.py
+++ variables.py
@@ -6,6 +6,8 @@ DOWNLOAD_API = "https://apis.googolplex.live/api/drive/download"
 
 FETCH_TV_SERIES = f"https://apis.googolplex.live/api/tvSeries/fetchSeriesDetails/{TV_ID}"
 
+SEARCH_API = 'https://apis.googolplex.live/api/movies/search/' # add keyword at the end e.g. search/the%20vampire
+
 # Movies not supported yet !!!!!!!!
 MOVIE_ID  = "616037"
 FETCH_MOVIE_DETAILS = f"https://apis.googolplex.live/api/movies/getAll/{MOVIE_ID}"
