ae38ad9af240c8a2b6af9b72be5b007e56faff64
Ashish Kandu <ashishkandu43@gmail.com>
2022-11-13 00:11:30 +0530

    changes in variable.py

--- variables.py
+++ variables.py
@@ -1,11 +1,11 @@
 # Series config
 TV_ID = "1399"
 
-# Movies not supported yet !!!!!!!!
-MOVIE_ID  = "616037"
-
 LOGIN_API = 'https://apis.googolplex.live/api/auth/signin'
 DOWNLOAD_API = "https://apis.googolplex.live/api/drive/download"
 
 FETCH_TV_SERIES = f"https://apis.googolplex.live/api/tvSeries/fetchSeriesDetails/{TV_ID}"
+
+# Movies not supported yet !!!!!!!!
+MOVIE_ID  = "616037"
 FETCH_MOVIE_DETAILS = f"https://apis.googolplex.live/api/movies/getAll/{MOVIE_ID}"
